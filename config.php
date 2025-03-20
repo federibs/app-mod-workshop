d416e2a6025ffb91ccd4a1186fd01b01d14e6551
Federico Riboldi <federicoriboldi91@gmai.com>
2025-03-20 15:37:49 +0000

    asd

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration with ENV variables. Set default values as well 
-$db_host = getenv('DB_HOST') ?: 'localhost';
-$db_name = getenv('DB_NAME') ?: 'image_catalog';
-$db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
-$db_pass = getenv('DB_PASS') ?: 'wrong_password';
+$db_host = getenv('DB_HOST');
+$db_name = getenv('DB_NAME');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
 // Note getenv() is PHP 5.3 compatible
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
